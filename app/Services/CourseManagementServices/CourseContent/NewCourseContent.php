ccded266bdda6248af8045c221ae2cdf9b4fedd5
Emmanuel Coker <emmanuelcoker420@gmail.com>
2022-08-31 14:42:05 +0100

    encapsulating updateContent by creating smaller methods

--- app/Services/CourseManagementServices/CourseContent/NewCourseContent.php
+++ app/Services/CourseManagementServices/CourseContent/NewCourseContent.php
@@ -15,6 +15,13 @@ class NewCourseContent
     public $curriculumId;
     public $content_order;
     public $fileName;
+    private $pathToVideo = '/videos';
+    private $pathToDelVideo = 'videos/';
+
+    private $pathToDocs = '/documents';
+    private $pathToDelDocs = 'documents/';
+
+    private $contentable_type;
 
     public function __construct($file, $curriculumId, $content_order = 1){
         $this->file = $file;
@@ -22,13 +29,12 @@ class NewCourseContent
         $this->content_order = $content_order;
     }
 
-
-    public function createContent(){
-        $uploadService = new UploadService($this->file);
+//creating and uploading content
+    public function storeContentFile($file){
+        $uploadService = new UploadService($file);
         $fileType = $uploadService->checkFileType();
         $fileExtension = $uploadService->getExtension();
         
-
         if($fileType == 'video'){
             $contentable_type = 'App\Models\Video';
             $fileNameToStore = $uploadService->uploadFile('/videos');
@@ -40,16 +46,23 @@ class NewCourseContent
             $fileNameToStore = $uploadService->uploadFile('/documents');
             $newContent = Self::createDocContent($fileNameToStore, $fileExtension);
         }
-        
+
+        return ['newContent' => $newContent, 'contentable_type' => $contentable_type];
+    }
+
+    public function createContent(){
+        $newContent = Self::storeContentFile($this->file);
+
         $courseContent = CourseContent::create([
             'curriculum_id'    => $this->curriculumId,
-            'contentable_id'   => $newContent->id,
-            'contentable_type' => $contentable_type,
+            'contentable_id'   => $newContent['newContent']->id,
+            'contentable_type' => $newContent['contentable_type'],
             'content_order'    => $this->content_order
         ]);
 
         return $courseContent;
     }
+
 
     //create video content
     public function createVideoContent($fileNameToStore, $fileExtension){
@@ -88,108 +101,125 @@ class NewCourseContent
 
     public function updateContent($id){
         $courseContent = CourseContent::findOrFail($id);
-
-        //compare old and new file types 
-        
+       
         $uploadService = new UploadService($this->file);
         $fileType = $uploadService->checkFileType();
-        $fileExtension = $uploadService->getExtension();
 
         $oldFileType = Self::checkContentableType($courseContent);
-        $content_order = $this->content_order = '' ? $this->content_order = 1 : $this->content_order; 
 
         //if new filetype is video
         if($fileType == 'video'){
             //if the new file type is video and the old file type is also video
             if($fileType == $oldFileType){
-                $contentable_type = 'App\Models\Video';
-
-                //delete old video and upload new one
-                $uploadService = new UploadService($this->fileName);
-                $videoDetails = $uploadService->updateVideo($courseContent, $this->file);
-
-                //update video record
-                $courseContent->contentable->update([
-                    'video_name' => $videoDetails['videoHashName'],
-                    'video_type' => $videoDetails['extension']
-                ]);
-
+                //update old video and content
+                Self::updateOldVideo($this->fileName, $this->file, $courseContent);
             }else{
                 //if the new file type is video and old file is document
-                $contentable_type = 'App\Models\Video';
-                
-                //delete the old contentable record
-                $courseContent->contentable->delete();
-
-                //delete the old document
-                $uploadService = new UploadService($this->fileName);
-                $uploadService->deleteFile('documents/');
-
-                //upload new video
-                $uploadService = new UploadService($this->file);
-                $fileNameToStore = $uploadService->uploadFile('/videos');
-
-                //create new video record
-                $newVideoContent = Self::createVideoContent($fileNameToStore, $fileExtension); 
-
-                //update the content record
-                $courseContent->update([
-                    'contentable_id'   => $newVideoContent->id,
-                    'contentable_type' => $contentable_type,
-                    'content_order'    => $content_order
-
-                ]);
+                $newVideoContent = Self::updateDocToVideo($this->fileName, $this->file, $courseContent, 
+                                                $this->pathToDelDocs, $this->pathToVideo);
             }
-
-            
         }
-
 
         //if new filetype is document
         if($fileType == 'document'){
             //if the new file is a document and the old file is a document also
             if($fileType == $oldFileType){
-                 $contentable_type = 'App\Models\CourseDoc';
-
-                //delete old file and upload new one
-                $uploadService = new UploadService($this->fileName);
-                $docDetails = $uploadService->updateDoc($courseContent, $this->file);
-
-                //update file record
-                $courseContent->contentable->update([
-                    'filename' => $docDetails['docHashName'],
-                    'filetype' => $docDetails['extension']
-                ]);
+                Self::updateOldDoc($this->fileName, $this->file, $courseContent);
             }else{
-                //if the new file type is document and old file is video
-                $contentable_type = 'App\Models\CourseDoc';
-                
-                //delete the old contentable record
-                $courseContent->contentable->delete();
-
-                //delete the old video
-                $uploadService = new UploadService($this->fileName);
-                $uploadService->deleteFile('videos/');
-
-                //upload new document
-                $uploadService = new UploadService($this->file);
-                $fileNameToStore = $uploadService->uploadFile('/documents');
-
-                //create new document record
-                $newDocContent = Self::createDocContent($fileNameToStore, $fileExtension); 
-
-                //update the content record
-                $courseContent->update([
-                    'contentable_id'   => $newDocContent->id,
-                    'contentable_type' => $contentable_type,
-                    'content_order'    => $content_order
-                ]);
+                //if the new file type is doc and the old is video
+                $newDocContent = Self::updateVideoToDoc($this->fileName, $this->file, $courseContent, 
+                                            $this->pathToDelVideo, $this->pathToDocs);
             }
         }
-
+            
+       
 
         return 'Content Updated Successfully';
     }
 
+
+    //changing the old video to new one
+    public function updateOldVideo($oldFile, $newFile, $model){
+        $uploadService = new UploadService($oldFile);
+        $videoDetails = $uploadService->updateVideo($model, $newFile);
+         //update video record
+         $model->contentable->update([
+            'video_name' => $videoDetails['videoHashName'],
+            'video_type' => $videoDetails['extension']
+        ]);
+    }
+
+    //changing the old document to a new one
+    public function updateOldDoc($oldFile, $newFile, $model){
+        //delete old file and upload new one
+        $uploadService = new UploadService($oldFile);
+        $docDetails = $uploadService->updateDoc($model, $newFile);
+
+        //update file record
+        $model->contentable->update([
+            'filename' => $docDetails['docHashName'],
+            'filetype' => $docDetails['extension']
+        ]);
+    }
+
+
+    public function updateDocToVideo($oldDocFile, $newVidFile, $model, $oldDocPath, $newVidPath){
+        //if the new file type is video and old file is doc
+        $contentable_type =  'App\Models\Video';
+        //delete the old contentable record
+        $model->contentable->delete();
+        $content_order = $this->content_order = '' ? $this->content_order = 1 : $this->content_order; 
+        Self::deleteFile($oldDocFile, $oldDocPath);
+
+        //upload new video
+        $uploadService = new UploadService($newVidFile);
+        $fileExtension = $uploadService->getExtension();
+        $fileNameToStore = $uploadService->uploadFile($newVidPath);
+
+        //create new video record
+        $newVidContent = Self::createVideoContent($fileNameToStore, $fileExtension); 
+         //update the content record
+         $model->update([
+            'contentable_id'   => $newVidContent->id,
+            'contentable_type' => $contentable_type,
+            'content_order'    => $content_order
+        ]);
+    }
+
+    public function updateVideoToDoc($oldVidFile, $newDocFile, $model, $oldVidPath, $newDocPath){
+        //if the new file type is document and old file is video
+        $contentable_type =  'App\Models\CourseDoc';
+        //delete the old contentable record
+        $model->contentable->delete();
+        $content_order = $this->content_order = '' ? $this->content_order = 1 : $this->content_order; 
+
+        Self::deleteFile($oldVidFile, $oldVidPath);
+
+        //upload new document
+        $uploadService = new UploadService($newDocFile);
+        $fileExtension = $uploadService->getExtension();
+        $fileNameToStore = $uploadService->uploadFile($newDocPath);
+
+        //create new document record
+        $newDocContent = Self::createDocContent($fileNameToStore, $fileExtension); 
+        $model->update([
+            'contentable_id'   => $newDocContent->id,
+            'contentable_type' => $contentable_type,
+            'content_order'    => $content_order
+        ]);
+    }
+
+    //upload new file
+    public function newFile($file, $path){
+        $uploadService = new UploadService($file);
+        $fileNameToStore = $uploadService->uploadFile($path);
+        return $fileNameToStore;
+    }
+
+    //delete file
+    public function deleteFile($file, $path){
+        $uploadService = new UploadService($file);
+        $uploadService->deleteFile($path);
+    }
 
 }
